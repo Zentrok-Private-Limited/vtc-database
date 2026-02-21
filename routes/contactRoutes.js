4225f1d2fe8007e2b7dbc55cff0e92566051f4a9
Babli Sharma <bablithv101@gmail.com.com>
2026-02-21 12:40:29 +0530

    today test1

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const Contact = require("../models/contact"); // ✅ small letter OK
+const Contact = require("../models/contact");
+const { addToGoogleSheet } = require("../services/googleSheet.service");
 
 router.post("/contact", async (req, res) => {
   try {
     const data = new Contact(req.body);
     await data.save();
 
-    console.log("✅ Saved to MongoDB");
+    await addToGoogleSheet(req.body);
 
-    res.status(200).json({ message: "Saved to MongoDB" });
+    res.status(200).json({
+      message: "Saved to MongoDB & Google Sheet ✅"
+    });
 
   } catch (err) {
-    console.error("❌ Route Error:", err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.get("/contact", async (req, res) => {
-  try {
-    const data = await Contact.find().sort({ createdAt: -1 });
-    res.json(data);
-  } catch (err) {
+    console.error("❌ ERROR:", err);
     res.status(500).json({ error: err.message });
   }
 });
